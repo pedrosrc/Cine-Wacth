171cf37f5390ce2013e7003810663489aa113a66
Pedro Leonardo <pedroleon.pc@hotmail.com>
2023-03-14 18:00:16 -0300

    Finish

--- cine-wacth-app/src/components/Header/index.jsx
+++ cine-wacth-app/src/components/Header/index.jsx
@@ -5,43 +5,42 @@ import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 export default function Header(){
 
-
     function menuShow(){
-
-
-       
-        
+        let bottom_open = document.querySelector('.open_bottom')
+        let bottom_close = document.querySelector('.close_bottom')
         let menuMobile = document.querySelector('.mobile-menu')
         if(menuMobile.classList.contains('open')){
             menuMobile.classList.remove('open')
-               
-        }
-        else{
+            bottom_open.style.display="block";
+            bottom_close.style.display="none";
+        }else{
             menuMobile.classList.add('open');
-            
-            
-
+            bottom_open.style.display="none";
+            bottom_close.style.display="block";
         }
     }
 
     return(
         <div className='container_nav'>
-            <h1><Link to='/'>CineWacth</Link></h1>
-            <ul>
-                <li className='nav_item'><Link to="/">HOME</Link></li>
-                <li className='nav_item'><Link to="/sobre">SOBRE</Link></li>
-                <li className='nav_item' id='list'><Link to="/lista">MINHA LISTA</Link></li>
-            </ul>
-            <div className="toglle-btn">
-                <button onClick={menuShow} className="icon_bottom" ><AiOutlineMenu/></button>
-            </div>
-            <div className='mobile-menu'>
-            <ul>
-                <li className='nav_item'><Link to="/">HOME</Link></li>
-                <li className='nav_item'><Link to="/sobre">SOBRE</Link></li>
-                <li className='nav_item' id='list'><Link to="/lista">MINHA LISTA</Link></li>
-            </ul>
-            </div>
+        <h1><Link to='/'>CineWacth</Link></h1>
+        <ul>
+            <li className='nav_item'><Link to="/">HOME</Link></li>
+            <li className='nav_item'><Link to="/sobre">SOBRE</Link></li>
+            <li className='nav_item' id='list'><Link to="/lista">MINHA LISTA</Link></li>
+        </ul>
+        <div className="toglle-btn">
+            <button onClick={menuShow} className="icon_bottom">
+            <AiOutlineMenu  className='open_bottom'/>
+            <AiOutlineClose className='close_bottom'/>
+            </button>
         </div>
+        <div className='mobile-menu'>
+        <ul>
+            <li className='nav_item'><Link to="/">HOME</Link></li>
+            <li className='nav_item'><Link to="/sobre">SOBRE</Link></li>
+            <li className='nav_item' id='list'><Link to="/lista">MINHA LISTA</Link></li>
+        </ul>
+        </div>
+    </div>
     )
 }
--- cine-wacth-app/src/pages/Sobre/index.jsx
+++ cine-wacth-app/src/pages/Sobre/index.jsx
@@ -6,7 +6,7 @@ export default function Sobre(){
         <div className="container_sobre">
             <div className="sobre">
             <h1>Sobre</h1>
-            <p>CineWacth se trata de um projeto pessoal, onde o intuinto é mostrar meus conhecimentos, e nada comercial. A ideia desse projeto é do <a href="https://www.udemy.com/course/curso-nodejs/#instructor-1" target="_blank">Matheus Fraga</a>, feito na Udemy, onde está sendo consumido uma API da <a href="https://developers.themoviedb.org/3" target="_blank">TheMovieDB </a>e podemos ver os filmes em cartazes no cinema no Brasil e ver detalhes e favorita-los. Modifiquei e aprimorei partes desse projeto, espero que gostem e me acompanhem nas redes socias:</p>
+            <p>CineWacth se trata de um projeto pessoal, criado por <a href="https://github.com/pedrosrc" target="_blamnk">Pedro Leonardo</a>, onde o intuinto é mostrar meus conhecimentos. A ideia desse projeto foi apresentada por <a href="https://www.udemy.com/course/curso-nodejs/#instructor-1" target="_blank">Matheus Fraga</a>. Nesse projeto está sendo consumido uma API da <a href="https://developers.themoviedb.org/3" target="_blank">TheMovieDB</a>, onde podemos ver os filmes em cartazes no cinema no Brasil e ver detalhes e favorita-los. Modifiquei e aprimorei partes desse projeto, espero que gostem e me acompanhem nas redes socias:</p>
             <ul>
                 <li>
                     <a href="https://github.com/pedrosrc" target="_blamnk"><TbBrandGithub/></a></li>
